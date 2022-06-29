24b7645f3e0b83fb1f7ed6cee38d7856e7e2ca9b
Artemka374 <fomiuk.artemka123@gmail.com>
2022-06-30 00:45:34 +0300

    update assembler to use TokenStream

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,4 +1,7 @@
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    str::FromStr,
+};
 
 use crate::structures::*;
 use convert_case::{
@@ -10,7 +13,7 @@ use quote::*;
 
 /// Assembles ink! contract from the parsed contract struct and return it as a vec of Strings
 pub fn assemble_contract(contract: Contract) -> TokenStream {
-    let contract_name = contract.name.to_case(Case::Snake);
+    let contract_name = format_ident!("{}", contract.name.to_case(Case::Snake));
     let signature = signature();
     let imports = assemble_imports(contract.imports);
     let events = assemble_events(contract.events);
@@ -20,7 +23,7 @@ pub fn assemble_contract(contract: Contract) -> TokenStream {
     let constructor = assemble_constructor(contract.constructor);
     let functions = assemble_functions(contract.functions);
 
-    let ink_contract = quote!(
+    let ink_contract = quote! {
         #signature
 
         #![cfg_attr(not(feature = "std"), no_std)]
@@ -37,15 +40,15 @@ pub fn assemble_contract(contract: Contract) -> TokenStream {
                 #functions
             }
         }
-    );
+    };
 
     ink_contract
 }
 
 /// Assembles ink! interface(trait) from the parsed interface struct and return it as a vec of Strings
 pub fn assemble_interface(interface: Interface) -> TokenStream {
-    let interface_name = interface.name;
-    let interface_name_ref = format!("{}Ref", interface_name);
+    let interface_name = format_ident!("{}", interface.name);
+    let interface_name_ref = format_ident!("{}Ref", interface.name);
     let signature = signature();
     let imports = assemble_imports(interface.imports);
     let events = assemble_events(interface.events);
@@ -53,7 +56,7 @@ pub fn assemble_interface(interface: Interface) -> TokenStream {
     let structs = assemble_structs(interface.structs);
     let function_headers = assemble_function_headers(interface.function_headers);
 
-    let ink_contract = quote!(
+    let ink_contract = quote! {
         #signature
         #imports
         #events
@@ -67,147 +70,189 @@ pub fn assemble_interface(interface: Interface) -> TokenStream {
         pub trait #interface_name {
             #function_headers
         }
-    );
+    };
 
     ink_contract
 }
 
 /// Sorts the imports inside the HashSet and return it as a Vec of Strings
 fn assemble_imports(imports: HashSet<String>) -> TokenStream {
+    let mut output = TokenStream::new();
     let mut output_vec = Vec::from_iter(imports);
     output_vec.sort();
-    quote!(#(#output_vec)"\n"*)
+
+    for import in output_vec {
+        output.extend(TokenStream::from_str(&import).unwrap());
+    }
+
+    output
 }
 
 /// Assembles ink! enums from the vec of parsed Enum structs and return them as a vec of Strings
 fn assemble_enums(enums: Vec<Enum>) -> TokenStream {
-    let mut output_vec = Vec::<String>::new();
+    let mut output = TokenStream::new();
 
     for enumeration in enums.iter() {
-        output_vec.push(format!("pub enum {} {{\n", enumeration.name));
+        let enum_name = format_ident!("{}", enumeration.name);
+        let mut values = TokenStream::new();
+
         for value in enumeration.values.iter() {
-            output_vec.push(format!("\t{}, \n", value));
+            let value_ident = format_ident!("{}", value);
+
+            values.extend(quote! {
+                #value_ident,
+            });
         }
-        output_vec.push(String::from("}\n\n"));
+
+        output.extend(quote! {
+            pub enum #enum_name {
+                #values
+            }
+        });
     }
-    quote!(#(#output_vec)*)
+
+    output
 }
 
 /// Assembles ink! events from the vec of parsed Event structs and return them as a vec of Strings
 fn assemble_events(events: Vec<Event>) -> TokenStream {
-    let mut output_vec = Vec::<String>::new();
+    let mut output = TokenStream::new();
 
     for event in events.iter() {
-        output_vec.push(String::from("#[ink(event)]\n"));
-        output_vec.push(format!("pub struct {} {{\n", event.name));
+        let event_name = format_ident!("{}", event.name);
+        let mut event_fields = TokenStream::new();
+
         for event_field in event.fields.iter() {
             if event_field.indexed {
-                output_vec.push(String::from("\t#[ink(topic)]\n"));
+                event_fields.extend(quote! {
+                    #[ink(topic)]
+                });
             }
-            output_vec.push(format!(
-                "\t{}: {},\n",
-                event_field.name.to_case(Case::Snake),
-                event_field.field_type
-            ));
+
+            let event_field_name = format_ident!("{}", event_field.name);
+            let event_field_type = TokenStream::from_str(&event_field.field_type).unwrap();
+
+            event_fields.extend(quote! {
+                #event_field_name: #event_field_type,
+            });
         }
-        output_vec.push(String::from("}\n\n"));
+
+        output.extend(quote! {
+            #[ink(event)]
+            pub struct #event_name
+            {
+                #event_fields
+            }
+        });
     }
 
-    quote!(#(#output_vec)*)
+    output
 }
 
 /// Assembles ink! storage struct from the vec of parsed ContractField structs and return it as a vec of Strings
 fn assemble_storage(contract_name: String, fields: Vec<ContractField>) -> TokenStream {
-    let mut output_vec = Vec::<String>::new();
+    let mut output = TokenStream::new();
+    let contract_name = format_ident!("{}", contract_name.to_case(Case::Snake));
 
-    output_vec.push(String::from("#[ink(storage)]\n"));
-    output_vec.push(String::from("#[derive(Default, SpreadAllocate)]\n"));
+    let mut storage_fields = TokenStream::new();
 
-    output_vec.push(format!("pub struct {} {{\n", contract_name));
     for field in fields.iter() {
-        output_vec.push(format!("\t{}: {},\n", field.name, field.field_type));
+        let field_name = format_ident!("{}", field.name);
+        let field_type = TokenStream::from_str(&field.field_type).unwrap();
+        storage_fields.extend(quote! {
+            #field_name: #field_type,
+        });
     }
 
-    output_vec.push(String::from("}\n"));
-    output_vec.push(String::from("\n"));
+    output.extend(quote! {
+        #[ink(storage)]
+        #[derive(Default, SpreadAllocate)]
+        pub struct #contract_name {
+            #storage_fields
+        }
+    });
 
-    quote!(#(#output_vec)*)
+    output
 }
 
 /// Assembles ink! structs from the vec of parsed Struct structs and return them as a vec of Strings
 fn assemble_structs(structs: Vec<Struct>) -> TokenStream {
-    let mut output_vec = Vec::<String>::new();
+    let mut output = TokenStream::new();
 
     for structure in structs.iter() {
-        output_vec.push(String::from("#[derive(Default, Encode, Decode)]\n"));
-        output_vec.push(String::from(
-            "#[cfg_attr(feature = \"std\", derive(scale_info::TypeInfo))]\n",
-        ));
-        output_vec.push(format!("pub struct {} {{\n", structure.name));
+        let struct_name = format_ident!("{}", structure.name);
+        let mut struct_fields = TokenStream::new();
+
         for struct_field in structure.fields.iter() {
-            output_vec.push(format!(
-                "\tpub {}: {},\n",
-                struct_field.name, struct_field.field_type
-            ));
+            let struct_field_name = format_ident!("{}", struct_field.name);
+            let struct_field_type = TokenStream::from_str(&struct_field.field_type).unwrap();
+
+            struct_fields.extend(quote! {
+                #struct_field_name: #struct_field_type,
+            });
         }
-        output_vec.push(String::from("}\n\n"));
+        output.extend(quote! {
+            #[derive(Default, Encode, Decode)]
+            #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+            pub struct #struct_name {
+                #struct_fields
+            }
+        });
     }
 
-    quote!(#(#output_vec)*)
+    output
 }
 
 /// Assembles ink! cosntructor from the parsed Function struct and return it as a vec of Strings
 fn assemble_constructor(constructor: Function) -> TokenStream {
-    let mut output_vec = Vec::<String>::new();
-    // we do this so we dont put tab between each string that we insert (function params)
-    let mut header = String::new();
+    let mut output = TokenStream::new();
+    let mut params = TokenStream::new();
 
-    output_vec.push(String::from("\t#[ink(constructor)]\n"));
-    header.push_str("\tpub fn new(");
+    for param in constructor.header.params.iter() {
+        let param_name = format_ident!("{}", param.name);
+        let param_type = TokenStream::from_str(&param.param_type).unwrap();
 
-    header.push_str(
-        constructor
-            .header
-            .params
-            .iter()
-            .map(|param| format!("{}: {}", param.name, param.param_type))
-            .collect::<Vec<String>>()
-            .join(", ")
-            .as_str(),
-    );
-    header.push_str(") -> Self {\n");
-    output_vec.push(header);
+        params.extend(quote! {
+            #param_name: #param_type,
+        });
+    }
 
-    output_vec.push(String::from(
-        "\t\tink_lang::codegen::initialize_contract(|instance: &mut Self| {\n",
-    ));
+    let mut body = TokenStream::new();
+
     for statement in constructor.body.iter() {
         // TODO remove comments
-        output_vec.push(format!("\t\t\t// {}\n", statement.content));
+        let content = format!("\t\t\t// {}\n", statement.content);
+        body.extend(TokenStream::from_str(&content).unwrap());
     }
-    output_vec.push(String::from("\t\t})\n"));
-    output_vec.push(String::from("\t}\n\n"));
 
-    quote!(#(#output_vec)*)
+    output.extend(quote! {
+        #[ink(constructor)]
+        pub fn new(#params) -> Self{
+            ink_lang::codegen::initialize_contract(|instance: &mut Self| {
+                #body
+            })
+        }
+    });
+
+    output
 }
 
 /// Assembles ink! functions from the vec of parsed Function structs and return them as a vec of Strings
 fn assemble_functions(functions: Vec<Function>) -> TokenStream {
-    let mut output_vec = Vec::<String>::new();
-    // we do this so we dont put tab between each string that we insert (function params)
-    let mut header: String;
+    let mut output = TokenStream::new();
 
     for function in functions.iter() {
-        header = String::new();
+        header = TokenStream::new();
         if function.header.external {
-            output_vec.push(format!(
+            let msg = format!(
                 "\t#[ink(message{})]\n",
                 if function.header.payable {
                     String::from(", payable")
                 } else {
                     String::from("")
                 }
-            ));
+            );
+            header.extend(TokenStream::from_str(&msg).unwrap());
         }
         header.push_str(
             format!(
@@ -233,9 +278,18 @@ fn assemble_functions(functions: Vec<Function>) -> TokenStream {
             )
             .as_str(),
         );
+
+        let mut params = TokenStream::new();
+
         for param in function.header.params.iter() {
-            header.push_str(format!(", {}: {}", param.name, param.param_type).as_str());
+            let param_name = format_ident!("{}", param.name);
+            let param_type = TokenStream::from_str(&param.param_type).unwrap();
+
+            params.extend(quote! {
+                #param_name: #param_type,
+            });
         }
+
         header.push_str(")");
         // return params
         if !function.header.return_params.is_empty() {
@@ -274,7 +328,7 @@ fn assemble_functions(functions: Vec<Function>) -> TokenStream {
         output_vec.push(String::from("\n"));
     }
 
-    quote!(#(#output_vec)*)
+    quote! {#(#output_vec)*}
 }
 
 /// Assembles ink! trait function headers from the vec of parsed FunctionHeader structs and return them as a vec of Strings
@@ -338,17 +392,17 @@ fn assemble_function_headers(function_headers: Vec<FunctionHeader>) -> TokenStre
         output_vec.push(header.to_owned());
     }
 
-    quote!(#(#output_vec)*)
+    quote! {#(#output_vec)*}
 }
 
 /// Adds a signature to the beginning of the file :)
 fn signature() -> TokenStream {
     const VERSION: &str = env!("CARGO_PKG_VERSION");
-    let version = format!("// Generated with Sol2Ink v{}\n", VERSION);
-    let link = String::from("// https://github.com/Supercolony-net/sol2ink\n\n");
-    quote!(
+    let version = format!("Generated with Sol2Ink v{}", VERSION);
+    let link = String::from("https://github.com/Supercolony-net/sol2ink");
+    quote! {
         // Generated with Sol2Ink v\n
         #[doc = #version]
         #[doc = #link]
-    )
+    }
 }
